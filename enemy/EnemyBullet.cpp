59784fb0e4f161a8e2a5dbdf7d1cfd1a8b54b88a
Lie Shiki <krugerjurgen337@gmail.com>
2023-06-15 00:41:17 +0900

    確認課題

--- enemy/EnemyBullet.cpp
+++ enemy/EnemyBullet.cpp
@@ -9,10 +9,15 @@ void EnemyBullet::Initialize(Model* model, const Vector3& position, const Vector
 	model_ = model;
 	worldTransform_.Initialize();
 	worldTransform_.translation_ = position;
+	worldTransform_.scale_ = {0.5f, 0.5f, 3.0f};
 	velocity_ = velocity;
+
+	worldTransform_.rotation_.y = std::atan2(velocity_.x, velocity_.z);
+	float velociteXZ = Length({velocity_.x, 0.0f, velocity_.z});
+	worldTransform_.rotation_.x = std::atan2(-velocity_.y, velociteXZ);
 }
 
-void EnemyBullet::Update() { 
+void EnemyBullet::Update() {
 	worldTransform_.UpdateMatrix();
 	worldTransform_.translation_ = Add(worldTransform_.translation_, velocity_);
 
@@ -21,6 +26,6 @@ void EnemyBullet::Update() {
 	}
 }
 
-void EnemyBullet::Draw(const ViewProjection& viewProjection) { 
+void EnemyBullet::Draw(const ViewProjection& viewProjection) {
 	model_->Draw(worldTransform_, viewProjection, textureHandle_);
 }
