454a35ff75229c828c6c3e943ec3adfab39a8c61
Lie Shiki <krugerjurgen337@gmail.com>
2023-07-21 10:52:27 +0900

    02_10

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -163,12 +163,13 @@ void GameScene::CheckAllCollision() {
 	}
 
 	std::list<Collider*>::iterator itrA = colliders_.begin();
-
 	for (; itrA != colliders_.end(); ++itrA) {
+
 		std::list<Collider*>::iterator itrB = itrA;
 		++itrB;
 
 		for (; itrB != colliders_.end(); ++itrB) {
+
 			CheckCollisionPair(*(itrA), *(itrB));
 		}
 	}
@@ -176,21 +177,20 @@ void GameScene::CheckAllCollision() {
 
 void GameScene::CheckCollisionPair(Collider* colliderA, Collider* colliderB) {
 	if (!(colliderA->GetCollisionAttribute() & colliderB->GetCollisionMask()) ||
-		!(colliderB->GetCollisionAttribute() & colliderA->GetCollisionMask())) {
+	    !(colliderB->GetCollisionAttribute() & colliderA->GetCollisionMask())) {
 		return;
 	}
-	
+
 	Vector3 posA = colliderA->GetWorldPosition();
 	Vector3 posB = colliderB->GetWorldPosition();
-
+	
 	float radA = colliderA->GetRadius();
 	float radB = colliderB->GetRadius();
-
+	
 	Vector3 Distance = {
 	    (posB.x - posA.x) * (posB.x - posA.x), (posB.y - posA.y) * (posB.y - posA.y),
-	    (posB.z - posA.z) * (posB.z - posA.z)
-	};
-
+	    (posB.z - posA.z) * (posB.z - posA.z)};
+	
 	if (Distance.x + Distance.y + Distance.z <= (radA + radB) * (radA + radB)) {
 		colliderA->OnCollision();
 		colliderB->OnCollision();
