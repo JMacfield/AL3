1b732aae6c31b087014cfd4c92f1ebc1b6437a73
Lie Shiki <krugerjurgen337@gmail.com>
2023-10-07 20:30:55 +0900

    aa

--- player/Player.cpp
+++ player/Player.cpp
@@ -4,11 +4,12 @@
 #include <Xinput.h>
 #include <cassert>
 
-// 
+// 弧度法変換
 #ifndef M_PI
 #define M_PI 3.14
 #endif
 
+// 変換
 #define deg_to_rad(deg) (deg * (M_PI/180))
 
 void Player::Initialize(Model* model) { 
@@ -21,6 +22,7 @@ void Player::Initialize(Model* model) {
 }
 
 void Player::Update() { 
+	// プレイヤーの挙動はMove関数にまとまってる
 	Move();
 
 	worldTransform_.UpdateMatrix();
@@ -34,27 +36,35 @@ void Player::Move() {
 	Vector3 move = {0, 0, 0};
 
 	float speed = 1.0f;
+	float angle;
+	float rad;
 
+	// 左方向へ移動（現在３０°程度）
 	if (input_->PushKey(DIK_A)) {
-		float angle = 215.0f;
-		float rad = (float)angle * (float)deg_to_rad(angle);
+		angle = 215.0f;
+		rad = (float)angle * (float)deg_to_rad(angle);
 		Vector3 direction = {cosf(rad), sinf(rad), 0};
 		move = direction * speed;
 	}
 
+	// 右方向へ移動（現在３０°程度）
 	if (input_->PushKey(DIK_D)) {
-		float angle = 260.0f;
-		float rad = (float)angle * (float)deg_to_rad(angle);
+		angle = 260.0f;
+		rad = (float)angle * (float)deg_to_rad(angle);
 		Vector3 direction = {cosf(rad), sinf(rad), 0};
 		move = direction * -speed;
 	}
+
+	// 
 
 	if (input_->PushKey(DIK_SPACE)) {
 		worldTransform_.translation_.y -= 10.0f;
 	}
 
+	// 常に下へ自由落下
 	worldTransform_.translation_.y -= 0.2f;
 
+	// 更新した座標を追加計算
 	worldTransform_.translation_ = Add(worldTransform_.translation_, move);
 	worldTransform_.matWorld_ = MakeAffineMatrix(
 	    worldTransform_.scale_, worldTransform_.rotation_, worldTransform_.translation_);
